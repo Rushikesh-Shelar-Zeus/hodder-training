445d0d5b5c9fcb04f1ff3e19f2fcb3949e37d440
Rushikesh Shelar <Rushikesh.Shelar@zeuslearning.mumbai>
2025-07-29 16:31:57 +0530

    init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 5;
+int b = 5;
+
+int c = a + b;
+bool isGrater = c > 10;
+if (isGrater)
+{
+    Console.WriteLine($"C is Grater than 10 its value is {c}");
+}
+else
+{
+    Console.WriteLine("C is LESS than 10");
+}
