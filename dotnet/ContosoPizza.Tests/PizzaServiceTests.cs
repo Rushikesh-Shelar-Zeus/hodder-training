9b9577f722011cec24c31493901a905f6ce807eb
Rushikesh-Shelar-Zeus <rushikesh.shelar@zeuslearning.com>
2025-08-04 19:13:32 +0530

    Create XUnit Test proj

---
+++ dotnet/ContosoPizza.Tests/PizzaServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using ContosoPizza.Services;
+using ContosoPizza.Models;
+
+
+namespace ContosoPizza.Tests;
+
+public class PizzaServiceTests
+{
+    [Fact]
+    public void GetPizzaCount_ReturnsCorrectCount()
+    {
+
+    }
+}
