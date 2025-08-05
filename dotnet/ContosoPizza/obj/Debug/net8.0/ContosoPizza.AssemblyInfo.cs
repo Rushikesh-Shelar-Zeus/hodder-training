19224b20b7dea3340846fd18085ab526fe5a0c83
Rushikesh-Shelar-Zeus <rushikesh.shelar@zeuslearning.com>
2025-08-05 16:58:47 +0530

    Gitignore update

--- dotnet/ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ dotnet/ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dae33c7fc5e6803b5dabd84c2b1e9b0d0d07fb46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29f59664816efcc4e1afd4cff606e29849021918")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
